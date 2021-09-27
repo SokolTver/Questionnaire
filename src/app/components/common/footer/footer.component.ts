99e5e9521347c9c5686513f20061d2fe12d05295
Kamila_Zaeva <kamila_zaeva@epam.com>
2021-09-27 17:06:40 +0300

    added some review comments

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// https://angular.io/guide/styleguide#component-custom-prefix
+// почитай про именование компонентов
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
@@ -11,5 +13,6 @@ export class FooterComponent {
     this.footerText = `© ${new Date().getFullYear()}`;
   }
 
+  // переменные обычно над конструктором описываются
   footerText: string;
 }
